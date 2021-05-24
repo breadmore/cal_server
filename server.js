0c979335b09544cbf0a66d8e47342a718c1948c9
lepo3 <lepo3@naver.com>
2021-05-24 12:23:27 +0900

    Initial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+const router = require("./routes/router");
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+//app.use(express.static(path.join(__dirname, '..', 'public/')));
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+app.use("/", router);
+
+
+app.listen(PORT, () => {
+    console.log(`Server On : localhost:${PORT}/`);
+})
